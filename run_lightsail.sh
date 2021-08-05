e63289035fe15f9d8b38d57058942b5536d596e3
Adam Molner <molnerad@gmail.com>
2021-08-05 17:34:20 -0400

    Create run_lightsail.sh

---
+++ run_lightsail.sh
@@ -0,0 +1,1 @@
+aws lightsail create-instances --instance-names "MyInstanceName" --availability-zone us-east-2a --blueprint-id amazon_linux_2 --bundle-id nano_2_0 --user-data file://k3s_kasten_install.sh
